787d17386f2defbb78ad9090443418c2d96eee44
julien gusen <gysenjulien@gmail.com>
2021-07-30 10:19:43 +0200

    add move function

--- Src/Player.lua
+++ Src/Player.lua
@@ -4,12 +4,35 @@ function Player:new()
   Player.super.new(self)
 end
 
+function Player:move(dt)
+  print(self.x)
+  print(self.speed)
+
+  if love.keyboard.isDown("right") then
+    self.x = self.x + self.speed * dt;
+  end
+
+  if love.keyboard.isDown("left") then
+    self.x = self.x - self.speed * dt;
+  end
+
+  if love.keyboard.isDown("down") then
+    self.y = self.y + self.speed * dt;
+  end
+
+  if love.keyboard.isDown("up") then
+    self.y = self.y - self.speed * dt;
+  end
+end
+
 function Player:load()
 
 end
 
 function Player:update(dt)
   Player.super.update(dt)
+  self:move(dt)
+
 end
 
 function Player:draw()
